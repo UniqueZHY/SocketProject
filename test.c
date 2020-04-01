fd054d297e4db33383b4a6c424c07ef43e12c1d8
UniqueZHY <782843253@qq.com>
2020-04-01 15:05:31 +0800

    test

---
+++ test.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年04月01日 星期三 15时02分49秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int main() {
+    int a, b;//注释
+    cin >> a >> b;
+    cout << a << b << endl;
+    return 0;
+}
